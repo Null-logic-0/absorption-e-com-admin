d3d2ac368736235716af352fb02ec85509e04582
Luka Tchelidze <157907877+Null-logic-0@users.noreply.github.com>
2024-12-17 14:05:01 +0400

    feat:created login form

---
+++ src/Components/AuthForm.js
@@ -0,0 +1,33 @@
+import FormSubmit from "./FormSubmit";
+import Input from "./Input";
+
+function AuthForm() {
+  return (
+    <div className="min-h-screen grid grid-cols-[48rem] content-center justify-center ">
+      <h1 className="font-extrabold text-white text-2xl bg-[#000000] p-2 text-center">
+        The Absorption Company.
+      </h1>
+      <form className="bg-[#fafafa] flex flex-col gap-8 p-5 rounded-b">
+        <Input
+          type="email"
+          name="email"
+          id="email"
+          htmlFor="email"
+          label="Email adress"
+        />
+
+        <Input
+          type="password"
+          name="password"
+          id="password"
+          htmlFor="password"
+          label="Password"
+        />
+
+        <FormSubmit />
+      </form>
+    </div>
+  );
+}
+
+export default AuthForm;
---
+++ src/Components/FormSubmit.js
@@ -0,0 +1,12 @@
+function FormSubmit() {
+  return (
+    <button
+      type="submit"
+      className="bg-[#000000] p-2 rounded w-full text-white font-semibold text-lg hover:bg-[#4557ff] cursor-pointer"
+    >
+      Login
+    </button>
+  );
+}
+
+export default FormSubmit;
